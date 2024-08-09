2ac05230079e0668be2dcb69bc4f753c2c2567a0
ba2sakal <sakalbatuhan1@git config --global user.name Your>
2024-08-09 17:00:55 +0200

    source setup bash entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # source ROS workspace
-source /opt/ros/$ROS_DISTRO/setup.bash
+source /opt/ros/$ROS_DISTRO/install/setup.bash
 [[ -f $WORKSPACE/devel/setup.bash ]] && source $WORKSPACE/devel/setup.bash
 [[ -f $WORKSPACE/install/setup.bash ]] && source $WORKSPACE/install/setup.bash
 
